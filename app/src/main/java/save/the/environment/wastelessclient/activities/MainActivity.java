d175ea9fc3e34444e0e291e622f0ee09c4f3b126
Rasmus Edvardsen <edvardsen.rasmus@gmail.com>
2018-06-17 15:02:58 +0200

    jobscheduler abstracted out, only initialized if null or no jobs in constant.

--- app/src/main/java/save/the/environment/wastelessclient/activities/MainActivity.java
+++ app/src/main/java/save/the/environment/wastelessclient/activities/MainActivity.java
@@ -7,11 +7,12 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import save.the.environment.wastelessclient.R;
 import save.the.environment.wastelessclient.services.ScheduledJobService;
+
+import static save.the.environment.wastelessclient.miscellaneous.Constants.jobScheduler;
 
 public class MainActivity extends Activity {
 
@@ -23,24 +24,17 @@ public class MainActivity extends Activity {
         Button scanBtn = findViewById(R.id.scanBtn);
         Button inventoryBtn = findViewById(R.id.inventoryBtn);
 
-
-
-        JobScheduler jobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
-        JobInfo.Builder builder =
-                new JobInfo.Builder(1,
-                    new ComponentName(getPackageName(),
-                        ScheduledJobService.class.getName()))
-                .setPeriodic(1000 * 60 * 60 * 8);
-        if((jobScheduler != null ? jobScheduler.schedule(builder.build()) : 0) == JobScheduler.RESULT_FAILURE){
-            //TODO: Do something here.
+        if (jobScheduler == null) {
+            jobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+            scheduleJob();
+        } else if (jobScheduler.getAllPendingJobs().isEmpty()){
+            scheduleJob();
         }
-
 
 
         scanBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("information", "yutxycglhvb");
                 startActivity(new Intent(MainActivity.this, BarcodeActivity.class));
             }
         });
@@ -50,5 +44,16 @@ public class MainActivity extends Activity {
                 startActivity(new Intent(MainActivity.this, InventoryActivity.class));
             }
         });
+    }
+
+    private void scheduleJob(){
+        JobInfo.Builder builder =
+                new JobInfo.Builder(1,
+                        new ComponentName(getPackageName(),
+                                ScheduledJobService.class.getName()))
+                        .setPeriodic(1000 * 60 * 60 * 8);
+        if ((jobScheduler != null ? jobScheduler.schedule(builder.build()) : 0) == JobScheduler.RESULT_FAILURE) {
+            //TODO: Do something here.
+        }
     }
 }
--- app/src/main/java/save/the/environment/wastelessclient/miscellaneous/Constants.java
+++ app/src/main/java/save/the/environment/wastelessclient/miscellaneous/Constants.java
@@ -1,8 +1,14 @@
 package save.the.environment.wastelessclient.miscellaneous;
 
+import android.app.job.JobScheduler;
+
 public class Constants {
     public static String baseURL = "https://wasteless20180406103633.azurewebsites.net";
     public static String usersPath = "/api/users";
     public static String scrapePath = "/api/scrape";
     public static String productsPath = "/api/products";
+
+
+
+    public static JobScheduler jobScheduler;
 }
